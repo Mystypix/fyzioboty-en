8f0014deb15dd9b8f0d539d00ab1eb026d18353a
Mystypix <jakub.smagin@gmail.com>
2023-02-16 22:42:49 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind()]
 });
